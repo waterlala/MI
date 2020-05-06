b7020207fa82000d7852dc7e3fbb3e086c5232af
waterlala <47877397+waterlala@users.noreply.github.com>
2020-05-06 22:49:39 +0800

    up

---
+++ Patient.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class patient():
+    def __init__(self,_id,gender,birthdate,deathdate):
+        self._id = _id
+        self._gender = gender
+        self._birthdate = birthdate
+        self._deathdate = deathdate
+
+
+# In[ ]:
+
+
+
+
---
+++ Record.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class record():
+    def __init__(self,_id,date,_type,area,dot,diseases,medicine):
+        self._id = _id
+        self._date = date
+        self._type = _type
+        self._area = area
+        self._dot = dot
+        self._diseases = diseases
+        self._medicine = medicine
+
+
+# In[ ]:
+
+
+
+
